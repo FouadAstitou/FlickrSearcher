66b3ed64f80cad06292068ea9ab40b66b0df5a6f
Fouad <f.astitou@gmail.com>
2016-08-27 18:32:10 +0200

    Created PhotoDataSourceClass. Added ViewController with a CollectionView and NAvigationController to the StoryBoard.

---
+++ FlickrSearecher/PhotoDataSource.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoDataSource.swift
+//  FlickrSearecher
+//
+//  Created by Supervisor on 27-08-16.
+//  Copyright © 2016 Nerdvana. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    
+    // Array to store the Flickr Photos
+    var flickrPhotos = [FlickrPhoto]()
+    
+    // MARK: - numberOfItemsInSection
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // MARK: - cellForItemAtIndexPath
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let identifier = "FlickrCell"
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(identifier, forIndexPath: indexPath)
+        
+        return cell
+    }
+}
---
+++ FlickrSearecher/PhotosViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PhotosViewController.swift
+//  FlickrSearecher
+//
+//  Created by Supervisor on 27-08-16.
+//  Copyright © 2016 Nerdvana. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet var collectionView: UICollectionView!
+    
+    // MARK: - Properties
+    let photoDataSource = PhotoDataSource()
+    
+    // MARK: - View Setup
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.dataSource = photoDataSource
+    }
+}
